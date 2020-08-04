c632c91199704a6c8b56ca50eb57cf48eaad9cdb
Michelle Slee <sleemichelle@gmail.com>
2020-08-04 16:04:57 +0100

    Updated components

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function List() {
+  return (
+    <ul>
+      <li>Coffee</li>
+      <li>Wine</li>
+      <li>Biscuits</li>
+    </ul>
+  )
+};
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import './styles.css';
 
-const img = "https://images.freeimages.com/images/large-previews/b3d/flowers-1375316.jpg";
-
-ReactDOM.render(
-  <div>
-  <h1 className="header"> I love the following:</h1>
-
-  <ul>
-  <li>Coffee</li>
-  <li>Wine</li>
-  <li>Biscuits</li>
-  </ul>
-  <div>
-    <img src={img} alt ="flower"/>
-  </div>
-
-
-  <p>Created by {`${fName} ${lName}`} </p>
-  <p>Copyright {year}</p>
-  </div>,
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
